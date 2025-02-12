3e8905d2c59e9f388d64391b56601f01ea6bb76d
jaehwa noh <shwoghk14@gmail.com>
2025-02-12 19:23:14 +0900

    Create IntArrayExtensions

---
+++ LazyScrollSdk/src/main/kotlin/starlightlab/jaehwa/lazyscrollsdk/extensions/IntArrayExtensions.kt
@@ -0,0 +1,12 @@
+package starlightlab.jaehwa.lazyscrollsdk.extensions
+
+fun IntArray.sumRange(start: Int = 0, until: Int): Int {
+    if (this.isEmpty()) {
+        return 0
+    }
+    var sum = 0
+    for (i in start..until) {
+        sum += this[i]
+    }
+    return sum
+}
