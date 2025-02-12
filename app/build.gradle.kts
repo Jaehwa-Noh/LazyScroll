e8478f591fa90bbf529597b0ea9631c20304af2b
jaehwa noh <shwoghk14@gmail.com>
2025-02-12 19:19:04 +0900

    Bump target sdk version.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "starlightlab.jaehwa.lazyscroll"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "starlightlab.jaehwa.lazyscroll"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
