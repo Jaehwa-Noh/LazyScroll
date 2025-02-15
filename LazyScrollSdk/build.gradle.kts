d24368a6e66fc0fe935afa350a5ea770122caace
Jaehwa Noh <shwoghk14@gmail.com>
2025-02-15 21:21:37 +0900

    Bump 0.0.3-beta

--- LazyScrollSdk/build.gradle.kts
+++ LazyScrollSdk/build.gradle.kts
@@ -24,7 +24,7 @@ mavenPublishing {
     coordinates(
         groupId = "io.github.jaehwa-noh",
         artifactId = "lazy-scroll",
-        version = "0.0.2"
+        version = "0.0.3-beta"
     )
 
     publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
