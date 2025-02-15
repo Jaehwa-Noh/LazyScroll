f0cbcf5230c5cc846bb58f3c5fa0e3bf6a821c1f
Jaehwa Noh <shwoghk14@gmail.com>
2025-02-15 21:10:32 +0900

    Add test screen.

--- app/src/main/kotlin/starlightlab/jaehwa/lazyscroll/MainActivity.kt
+++ app/src/main/kotlin/starlightlab/jaehwa/lazyscroll/MainActivity.kt
@@ -4,14 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import starlightlab.jaehwa.lazyscroll.ui.theme.LazyScrollTheme
+import starlightlab.jaehwa.lazyscrollsdk.ui.LazyScrollbarScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +29,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             LazyScrollTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    ScreenForTesting(
+                        orientation = Orientation.Horizontal,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +40,60 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun ScreenForTesting(
+    orientation: Orientation,
+    modifier: Modifier = Modifier,
+) {
+    val lazyListState = rememberLazyListState()
+    Box(
         modifier = modifier
-    )
+            .fillMaxSize()
+    ) {
+        if (orientation == Orientation.Vertical) {
+            LazyColumn(
+                state = lazyListState,
+                modifier = Modifier.fillMaxWidth(),
+            ) {
+                createItems()
+            }
+            LazyScrollbarScreen(lazyListState = lazyListState)
+        } else {
+            LazyRow(
+                state = lazyListState,
+                modifier = Modifier.fillMaxHeight(),
+            ) {
+                createItems()
+            }
+            LazyScrollbarScreen(lazyListState = lazyListState)
+        }
+    }
+}
+
+
+private fun LazyListScope.createItems() {
+    for (i in 0..100) {
+        item {
+            Text("$i")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun LazyScrollbarVerticalPreview() {
     LazyScrollTheme {
-        Greeting("Android")
+        ScreenForTesting(
+            orientation = Orientation.Vertical
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LazyScrollbarHorizontalPreview() {
+    LazyScrollTheme {
+        ScreenForTesting(
+            orientation = Orientation.Horizontal
+        )
     }
 }
