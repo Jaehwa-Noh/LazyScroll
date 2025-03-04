b4569b7bd15640bb6b0df19f386f9fbba67ab937
Jaehwa Noh <shwoghk14@gmail.com>
2025-03-04 16:37:49 +0900

    Create TestTags.kt

---
+++ app/src/main/kotlin/starlightlab/jaehwa/lazyscroll/testtags/TestTags.kt
@@ -0,0 +1,6 @@
+package starlightlab.jaehwa.lazyscroll.testtags
+
+internal const val THUMB_TEST_TAG = "lazyScroll:thumbTestTag"
+internal const val THUMB_TRACK_TEST_TAG = "lazyScroll:thumbTrackTestTag"
+
+internal const val LAZY_LIST_STATE_TEST_TAG = "lazyListStateTestTag"
